3b932ba21e1bf0b35646c22166deaf72e473fcf1
Josh <carlson.joshuaph@gmail.com>
2018-03-27 15:15:50 -0500

    fixed build issues again

--- utils/templates/index.js
+++
@@ -1,6 +0,0 @@
-//Central Export
-
-export {default as component} from './component.js'
-export {default as store} from './store.js'
-export {default as reducer} from './reducer.js'
-export {default as express} from './express.js'
